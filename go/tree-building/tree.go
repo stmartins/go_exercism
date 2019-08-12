0dcb6d3fe7990a319d97a6a5f6cc4383b1f42afd
stmartins <phaninho@live.fr>
2019-08-12 06:20:12 +0200

    add tree building

--- go/tree-building/tree.go
+++ go/tree-building/tree.go
@@ -3,7 +3,7 @@ package tree
 import "fmt"
 import "sort"
 
-//import "errors"
+import "errors"
 
 type Record struct {
 	ID, Parent int
@@ -14,25 +14,61 @@ type Node struct {
 	Children []*Node
 }
 
+func errorChecker(records []Record) error {
+
+	var err error
+
+	err = nil
+	if records[0].ID != 0 {
+		err = errors.New("records have no root")
+	} else if records[0].ID == 0 && records[0].Parent != 0 {
+		err = errors.New("records have Parent")
+	}
+
+	lowerID := -1
+	for _, rec := range records {
+		if (rec.ID - lowerID) > 1 {
+			err = errors.New("Non continuous")
+			break
+		} else if rec.ID != 0 && rec.ID == rec.Parent {
+			err = errors.New("direct find")
+			break
+		}
+		if lowerID != rec.ID {
+			lowerID = rec.ID
+		} else {
+			err = errors.New("Duplicate find")
+			break
+		}
+	}
+	return err
+}
+
 func Build(records []Record) (*Node, error) {
 
 	//	rootID := 0
 
 	if len(records) == 0 {
-		return nil, nil
+		return nil, nil //errors.New("record est vide")
 	}
+
 	fmt.Println("======start============")
-	fmt.Printf("records => ")
+	fmt.Printf("records before sort=> ")
 	fmt.Println(records)
 	sort.Slice(records, func(i, j int) bool {
-		fmt.Printf("i => %d et j =>%d\n", records[i].ID, records[j].ID)
 		return records[i].ID < records[j].ID
 	})
+	fmt.Printf("records after sort=> ")
+	fmt.Println(records)
+	err := errorChecker(records)
+	if err != nil {
+		return nil, err
+	}
 
 	fmt.Printf("len %d\n", len(records))
 	node := make([]Node, len(records))
 	for i, v := range records {
-		fmt.Printf("value [%d]\n", v)
+		fmt.Printf("index %d value [%d]\n", i, v)
 		node[i].ID = v.ID
 		if v.ID != v.Parent {
 			node[v.Parent].Children = append(node[v.Parent].Children, &node[v.ID])
