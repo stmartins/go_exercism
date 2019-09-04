c2d0980944523c8d32bd9db8981e52cd4cbbc721
stmartins <phaninho@live.fr>
2019-09-04 04:02:29 +0200

    add node.js  exercises

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,16 @@
+export const value = ([color1, color2]) => {
+    return (COLORS.indexOf(color1)* 10) + COLORS.indexOf(color2)
+};
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,5 @@
+export function twoFer(name){
+	if (name == undefined)
+		return "One for you, one for me."
+	return "One for " + name + " , one for me."
+}
