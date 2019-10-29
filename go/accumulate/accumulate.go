652b77db4edb472ec8e1bb10c4e97fe67a24e683
stmartins <phaninho@live.fr>
2019-10-29 22:19:28 +0100

    add accumulate

---
+++ go/accumulate/accumulate.go
@@ -0,0 +1,14 @@
+package accumulate
+
+import "fmt"
+
+func Accumulate(input []string, fctn func(string) string) []string {
+
+    var ret = []string{}
+
+    for _, v := range input {
+        ret = append(ret, fctn(v))
+    }
+
+    return ret;
+}
